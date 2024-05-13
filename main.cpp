137cb6ec577388869d8a204c87b2bf14a51c889c
banwoojin <qksejfrha@gmail.com>
2024-05-13 09:30:06 +0900

    코인등록함

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+	int main()
+{
+	int number1 = 0;
+	int number2 = 0;
+	std::cin >> number1;
+	std::cin >> number2;
+
+	std::cout << number1 + number2 << std::endl;
+	std::cout << number1 * number2 << std::endl;
+	std::cout << number1 / number2 << std::endl;
+	std::cout << number1 % number2 << std::endl;
+	std::cout << number1 - number2 << std::endl;
+
+
+	return 0;
+}
